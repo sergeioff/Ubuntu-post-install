c59dc2bbde23a47c80b6c01917fa6b82a925cb6a
SergeiOFF <sergeioff@mail.ru>
2016-02-28 22:38:45 +0200

    fonts fix

--- install-zsh+solarized.sh
+++ install-zsh+solarized.sh
@@ -3,6 +3,13 @@ sudo apt-get install zsh
 #install oh-my-zsh
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
+#copy dotfile
+cp dotfiles/.zshrc ~/
+
+#install fixed fonts
+git clone https://github.com/powerline/fonts.git
+fonts/install.sh
+
 #install ls color-theme
 cd ~/
 wget --no-check-certificate https://raw.github.com/seebi/dircolors-solarized/master/dircolors.ansi-dark
