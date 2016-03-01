c5016f76078079ae3664c0913256efed1d120a4b
Sergei <sergeioff@mail.ru>
2016-03-01 19:04:24 +0200

    dotFiles -> configFiles

--- modify-grub.sh
+++ modify-grub.sh
@@ -5,7 +5,7 @@ else cp /etc/default/grub .
 	echo 'GRUB_FORCE_HIDDEN_MENU="true"' >> grub
 	echo 'export GRUB_FORCE_HIDDEN_MENU' >> grub
 	mv grub /etc/default/
-	mv 30_os-prober /etc/grub.d/
+	mv configFiles/30_os-prober /etc/grub.d/
 	update-grub
 	echo '###############################'
 	echo '# Grub successfully modified! #'
