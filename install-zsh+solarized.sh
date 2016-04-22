88488193e2e6b8627cdf0aa30bf961cf8ab9e09e
SergeiOFF <sergeioff@mail.ru>
2016-04-22 09:31:36 -0400

    directory fix

--- install-zsh+solarized.sh
+++ install-zsh+solarized.sh
@@ -4,7 +4,7 @@ sudo apt-get install zsh
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 #copy dotfile
-cp dotfiles/.zshrc ~/
+cp configFiles/.zshrc ~/
 
 #install fixed fonts
 git clone https://github.com/powerline/fonts.git
@@ -13,6 +13,6 @@ fonts/install.sh
 #install ls color-theme
 cd ~/
 wget --no-check-certificate https://raw.github.com/seebi/dircolors-solarized/master/dircolors.ansi-dark
+
 mv dircolors.ansi-dark .dircolors
 
-echo "eval `dircolors ~/.dircolors`" >> ~/.zshrc
