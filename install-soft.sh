9ecefca2981e252b802cc068c4285e0d39e71cbc
SergeiOFF <sergeioff@mail.ru>
2016-05-09 11:53:54 +0300

    + tmux

--- install-soft.sh
+++ install-soft.sh
@@ -5,7 +5,7 @@ else apt-get purge unity-webapps-common
 	add-apt-repository ppa:webupd8team/java
 	add-apt-repository ppa:numix
 	apt-get update
-	apt-get install htop mc vim tree lynx gparted ubuntu-restricted-extras smplayer p7zip-full exfat-utils fbreader gnote gimp kamerka kazam skype synaptic indicator-multiload  unity-tweak-tool speedcrunch artha compizconfig-settings-manager compiz-plugins oracle-java8-installer numix-gtk-theme numix-icon-theme-circle redshift-gtk
+	apt-get install htop mc vim tmux tree lynx gparted ubuntu-restricted-extras smplayer p7zip-full exfat-utils fbreader gnote gimp kamerka kazam skype synaptic indicator-multiload  unity-tweak-tool speedcrunch artha compizconfig-settings-manager compiz-plugins oracle-java8-installer numix-gtk-theme numix-icon-theme-circle redshift-gtk
 
 	firefox https://www.dropbox.com/install?os=lnx https://www.jetbrains.com/idea/download/ https://atom.io/ https://desktop.telegram.org/
 	echo '####################################'
