588e5720c40a575a03c9cbd5eadd6752d9066552
Sergey <pogorelov.s.y@gmail.com>
2016-12-19 16:31:00 +0200

    added virtualbox, maven and sublime

--- install-soft.sh
+++ install-soft.sh
@@ -5,9 +5,9 @@ else apt-get purge unity-webapps-common
 	add-apt-repository ppa:webupd8team/java
 	add-apt-repository ppa:numix
 	apt-get update
-	apt-get install htop mc vim tmux tree lynx gparted ubuntu-restricted-extras smplayer p7zip-full exfat-utils fbreader gnote gimp kamerka kazam skype synaptic indicator-multiload  unity-tweak-tool speedcrunch artha compizconfig-settings-manager compiz-plugins oracle-java8-installer numix-gtk-theme numix-icon-theme-circle redshift-gtk soundconverter
+	apt-get install htop mc vim tmux tree lynx gparted ubuntu-restricted-extras smplayer p7zip-full exfat-utils fbreader gnote gimp kamerka kazam skype synaptic indicator-multiload  unity-tweak-tool speedcrunch artha compizconfig-settings-manager compiz-plugins oracle-java8-installer maven numix-gtk-theme numix-icon-theme-circle redshift-gtk soundconverter virtualbox
 
-	firefox https://www.dropbox.com/install?os=lnx https://www.jetbrains.com/idea/download/ https://atom.io/ https://desktop.telegram.org/
+	firefox https://www.dropbox.com/install?os=lnx https://www.jetbrains.com/idea/download/  https://www.sublimetext.com/3 https://desktop.telegram.org/
 	echo '####################################'
 	echo '# Software installed successfully! #'
 	echo '####################################'
