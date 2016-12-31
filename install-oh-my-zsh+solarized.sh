b545fbe84708fed62fbcb20be71c56265a331fd2
Sergey Pogoryelov <pogorelov.s.y@gmail.com>
2016-12-31 07:32:59 -0500

    new config for zsh

--- install-oh-my-zsh+solarized.sh
+++ install-oh-my-zsh+solarized.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 #install oh-my-zsh
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+#sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 #copy dotfile
-cp configFiles/.zshrc ~/
+#cp configFiles/.zshrc ~/
 
 #install fixed fonts
-git clone https://github.com/powerline/fonts.git
-fonts/install.sh
+#git clone https://github.com/powerline/fonts.git
+#fonts/install.sh
 
 #install solarized color-theme
-cd ~/
-wget --no-check-certificate https://raw.github.com/seebi/dircolors-solarized/master/dircolors.ansi-dark
+#cd ~/
+#wget --no-check-certificate https://raw.github.com/seebi/dircolors-solarized/master/dircolors.ansi-dark
 
-mv dircolors.ansi-dark .dircolors
+#mv dircolors.ansi-dark .dircolors
+echo "eval 'dircolors ~/.dircolors'" >> ~/.zshrc
 
-chsh -s /bin/zsh
+#chsh -s /bin/zsh
