cba871885bc45f06cf9109529ccc2e03618e9d6a
Sergey <pogorelov.s.y@gmail.com>
2016-12-31 15:54:22 +0200

    + radio stations adding script

---
+++ add-radioStations.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp configFiles/rhythmdb.xml ~/.local/share/rhythmbox/
+echo '######################################'
+echo '# Radio stations added successfully! #'
+echo '######################################'
