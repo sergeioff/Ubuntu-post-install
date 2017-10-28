889c6db7842ed5e7af52621e009ae56c9207571f
Sergey Pogoryelov <pogorelov.s.y@gmail.com>
2017-10-28 10:15:15 +0300

    updated links

--- install-soft.sh
+++ install-soft.sh
@@ -11,7 +11,7 @@ else apt-get purge unity-webapps-common
 
 	apt install zsh htop mc vim tmux tree lynx gparted vlc p7zip-full exfat-utils fbreader gimp kamerka kazam synaptic indicator-multiload speedcrunch artha oracle-java9-installer maven numix-gtk-theme numix-icon-theme-circle soundconverter virtualbox zram-config chromium-browser spotify-client telegram-desktop gnome-tweak-tool
 
-	echo "Don't forget to install: Dropbox, Atom, IntelliJ, PyCharm"
+	firefox "https://simplenote.com/" "https://www.dropbox.com/install-linux" "https://atom.io/download/deb" "https://www.jetbrains.com/idea/download/#section=linux" "https://www.jetbrains.com/pycharm/download/#section=linux"
 
 	echo '####################################'
 	echo '# Software installed successfully! #'
