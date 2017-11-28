4cfa7e483bac3c4e5a28a0a4483b01a569d7f418
Sergey Pogoryelov <pogorelov.s.y@gmail.com>
2017-11-28 13:18:13 -0500

    Reverted for ubuntu 16.04 and updated the Spotify key

--- install-soft.sh
+++ install-soft.sh
@@ -5,11 +5,11 @@ else apt-get purge unity-webapps-common
 
 	add-apt-repository ppa:webupd8team/java
 	add-apt-repository ppa:numix
-	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
 	echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 	apt update
 
-	apt install zsh htop mc vim tmux tree lynx gparted vlc p7zip-full exfat-utils fbreader gimp kamerka kazam synaptic indicator-multiload speedcrunch artha oracle-java9-installer maven numix-gtk-theme numix-icon-theme-circle soundconverter virtualbox zram-config chromium-browser spotify-client telegram-desktop gnome-tweak-tool
+	apt install zsh htop mc vim tmux tree lynx gparted vlc p7zip-full exfat-utils fbreader gimp kamerka kazam synaptic indicator-multiload speedcrunch artha oracle-java8-installer maven unity-tweak-tool compizconfig-settings-manager compiz-plugins redshift-gtk numix-gtk-theme numix-icon-theme-circle soundconverter virtualbox zram-config chromium-browser spotify-client gnome-tweak-tool
 
 	firefox "https://simplenote.com/" "https://www.dropbox.com/install-linux" "https://atom.io/download/deb" "https://www.jetbrains.com/idea/download/#section=linux" "https://www.jetbrains.com/pycharm/download/#section=linux"
 
